ff9c4b713a473f38af63f0888c8c881fed88bde8
oguzman <a.omargc@gmail.com>
2016-02-05 11:19:33 -0600

    Initial Commit

---
+++ MagmaCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagmaCalendar
+//
+//  Created by Omar Guzman on 2/5/16.
+//  Copyright © 2016 Omar Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
